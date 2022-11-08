ca27d3a6724d24a1ad9d50e6b817b7b34e8fb035
Airat Murtazin <ayrat.murtazin@mail.ru>
2022-11-08 19:11:04 +0300

    add css and html. and js variables

--- calculator/src/app.js
+++ calculator/src/app.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const allBtns = document.querySelectorAll(".btn");
+const percentage = document.querySelector(".percent");
+const eraseEverything = document.querySelector(".c");
+const del = document.querySelector(".delete");
+const substract = document.querySelector(".substract");
+const add = document.querySelector(".add");
+const equal = document.querySelector(".equal");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const plusMinus = document.querySelector(".plusMinus");
+const zero = document.querySelector(".zero");
+const dot = document.querySelector(".dot");
+const displayPanel = document.querySelector(".display");
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function display() {
+	allBtns.forEach(btn => {});
+}
+function addNums() {}
+addNums();
+function substractNums() {}
+function divideNums() {}
+function multiplyNums() {}
+function equalNums() {}
